18ddc857872f86b5f2910ecb0d41481ced9b623a
erlebach <gordon.erlebach@gmail.com>
2020-01-04 19:54:05 -0500

    Fixed features using pandas. real/pred profit seem correct, but r2_coef does not.
    R2_coef reaches 100% for some combination of parameters, and these combinations are
    different for different stocks. This is strange.

--- linear_regression_5.py
+++ linear_regression_5.py
@@ -66,15 +66,8 @@ class Trading:
         min_date = int(rec.name)
 
         # parenthesis and & are required for compound conditional
-        #min_date = u.addDays(self.break_date, self.nb_train_days)
-        print("break_date= ", break_date)
-        print("min_date= ", min_date)
-        print("nb_train_days= ", nb_train_days)
-
         train_df = df.loc[(df.index <= break_date) & (df.index >= min_date)]
         test_df  = df.loc[df.index  >  break_date]
-        print("train_df.shape: ", train_df.shape)
-        print("test_df.shape: ", test_df.shape)
 
         # training set based on close prices
         # labels are based on the close prices on the next day
@@ -85,15 +78,31 @@ class Trading:
 
         # Make sure that all features are in cols for the rest of the program to work properly!!
 
-        #cols = ['c','vol']   # cleans up code, and avoids repetition
         #cols = ['c','o']   # cleans up code, and avoids repetition
         cols = ['c','o','l','h','vol']   # cleans up code, and avoids repetition
-        cols = ['c']   # cleans up code, and avoids repetition
         cols = ['c','o','l','h']   # cleans up code, and avoids repetition
         cols = ['c', 'vol']   # cleans up code, and avoids repetition
+        cols = ['c']   # cleans up code, and avoids repetition
+
+        # reject the stock if the minimum 20-day average volume is less than 200,000 (over the testing
+        # and training period)
+        df['vol_sma_avg_20'] = df['vol'].rolling(window=20).mean()
+
+        train_vol = df['vol_sma_avg_20'].loc[(df.index <= break_date) & (df.index >= min_date)]
+        test_vol  = df['vol_sma_avg_20'].loc[df.index  >  break_date]
+
+        min_avg_train_vol = train_vol.min()
+        min_avg_test_vol  = test_vol.min()
+
+        #print('min_avg_train_vol= ', min_avg_train_vol)
+        #print('min_avg_test_vol= ',  min_avg_test_vol)
+
+        if min_avg_train_vol < 200000 or min_avg_test_vol < 200000:
+            return False
 
         train_df = train_df[cols]
         test_df  = test_df[cols]
+
 
         # days:  -2       -1          0
         #             yesterday     today
@@ -118,9 +127,9 @@ class Trading:
         # day[2]                 : rows[nb_feature_days-3 : -3]
         # day[nb_feature_days-1] : rows[0 : -nb_feature_days]
 
-        wait_days = 1
         train_day_d = []
         test_day_d  = []
+
         for i in range(0, nb_feature_days):
             j = nb_feature_days - 1 - i
             day = train_df.iloc[nb_feature_days-1-j : -1-j-wait_days]
@@ -140,85 +149,86 @@ class Trading:
         test_df  = pd.concat(test_day_d,  axis=1)
 
         # Remove all date columns except the first (column 1)
-        print(train_df.head(5))
-        print(test_df.head(5))
-        quit()
 
         # Volume is sometimes zero. Must impute its value. Use mean value over 3 days prior and three days following. How to do this/ 
+        # Closing prices are the first column
 
-        print(train_df.columns)
-        self.x_train = train_df.values
-        print("train_df.shape= ", train_df.shape)
-        self.y_train = train_df.iloc[wait_days:,0].values
-        self.y_train_a = train_df.loc[wait_days:,'c'].values
+        self.x_train = train_df.values[0:-wait_days, 1:]  # do not include date
+        self.y_train = train_df.loc[wait_days:,'c'].values # 
 
-        self.y_train = train_df.iloc[0:5:,:].values
-        self.y_train_a = train_df.loc[0:5:,:].values
+        self.x_test  = test_df.values[0:-wait_days, 1:] # 
+        self.y_test  = test_df.loc[wait_days:,'c'].values # 
 
-        print(self.y_train[0:5])
-        print()
-        print(self.y_train_a[0:5])
-        quit()
-        print("x_train.shape: ", self.x_train.shape)
-        print("y_train.shape: ", self.y_train.shape)
-        self.train_dates = train_df.index.values
-        self.x_test  = test_df.values[0:200] # 200 test values
-        self.test_dates = test_df.index.values
+        self.train_dates = train_df.iloc[:-wait_days,0].values
+        self.nb_train_days = self.train_dates.shape[0]
+        self.test_dates = test_df.iloc[:-wait_days,0].values
+        self.nb_test_days = self.test_dates.shape[0]
 
-        # MISSING: self.y_train and self.y_test
-
-        """
-        # Formula valid for any value of feature_days
-        lst = []
-        for n in range(nb_feature_days):  # [0,1,2]
-            m = (nb_feature_days - 1) - n   # m = nb_features-1 --> 0
-            # today should be the first list of features
-            xx = train_df[cols].values[m:-nb_feature_days+m + 1-wait_days]
-            lst.append(train_df[cols].values[m:-nb_feature_days+m + 1-wait_days])
-        self.x_train = np.hstack(lst) # stack along columns
-
-        # Training Label: Predict closing price the next day
-        self.y_train = train_df['c'].values[nb_feature_days-1+wait_days:]
-        self.train_dates = train_df.index.values[nb_feature_days-1:-wait_days]
-        print(self.x_train[0][0:3])
-        print(self.train_dates[0:3])  # WRONG!!
-        quit()
-        """
-    
-        """
-        # Manual creation of formula
-        x_train_1 = train_df[cols].values[0:-3]
-        x_train_2 = train_df[cols].values[1:-2]
-        x_train_3 = train_df[cols].values[2:-1]
-        x_train = np.hstack([x_train_1, x_train_2, x_train_3])  # stack along columns
-        print(x_train.shape)
-        """
-        
-        lst = []
-        for n in range(nb_feature_days):
-            m = (nb_feature_days - 1) - n
-            # today should be the first list of features
-            lst.append(test_df[cols].values[m:-nb_feature_days+m + 1-wait_days])
-        self.x_test = np.hstack(lst) # stack along columns
-    
-        # Test Label
-        self.y_test = test_df['c'].values[nb_feature_days-1+wait_days:]
-        self.test_dates = test_df.index[nb_feature_days-1:-wait_days]
-
-        print("nb train_dates: ", len(self.train_dates))
-        print("nb test_dates: ", len(self.test_dates))
-    
         n_features = self.x_train.shape[-1]
+
+        """
+        if (n_features == 1):
+            print("cannot handle a single feature. Exit")
+            #embed()
+            return False
+:y_train = train_df.loc[wait_days:,'c'].values
+        self.train_dates = train_df.iloc[:-wait_days,0]
+        self.nb_train_days = self.train_dates.shape[0]
+
+        self.x_test  = test_df.values[0:-wait_days, 1:] # 200 test values, do not include date
+        self.y_test  = test_df.loc[wait_days:,'c'].values # 200 test values
+        self.test_dates = test_df.iloc[:-wait_days,0].values
+        self.nb_test_days = self.test_dates.shape[0]
+
+        n_features = self.x_train.shape[-1]
+        """
     
         """
         if (n_features == 1):
             print("cannot handle a single feature. Exit")
-            embed()
-            quit()
+            #embed()
+            return False
         """
 
         self.feature_names = cols
-        target = 'price'
+
+        """
+        print("\n\n")
+        print("cols= ", cols)
+        print("train_df")
+        print(train_df.head())
+
+        print("y_train: ", self.y_train.shape)   # one rows to few!!!! << ERROR ERROR ERROR
+        print(self.y_train[0:5])
+        print("x_train.shape: ", self.x_train.shape)
+        print("y_train.shape: ", self.y_train.shape)
+        print("train_dates.shape: ", self.train_dates.shape)
+
+        print("\nself.train")
+        print(self.x_train[0:5,:])
+        print(self.y_train[0:5])
+        print(self.train_dates[0:5])
+        print(self.x_train.shape)
+        print(self.y_train.shape)
+        print(self.train_dates.shape)
+
+        # Shape self.x_train: [nrows,ncols]
+        # Shape self.y_train: [nrows,]
+
+        print("\nself.test")
+        print(self.x_test[0:5,:])
+        print(self.y_test[0:5])
+        print(self.test_dates[0:5])
+        print(self.x_test.shape)
+        print(self.y_test.shape)
+        print(self.test_dates.shape)
+
+        #quit()
+
+        # MISSING: self.y_train and self.y_test
+        """
+
+        return True
 
     #----------------------------------
     def processStock(self):
@@ -229,14 +239,15 @@ class Trading:
     
         # What is the first column? The number of features? 
         # If so, I must do a transpose on the data
-        print(self.x_train.shape, self.y_train.shape)
+        #print("x_train, y_train shapes: ", self.x_train.shape, self.y_train.shape)
         regr.fit(self.x_train, self.y_train)
-        print(self.x_train[0][0:3])
-        print(self.train_dates[0:3])
-        quit()
+        #print("x_train: ", self.x_train.shape)
+        #print("train_dates: ", self.train_dates[0:3])
     
         # Make predictions using the testing set
+        #print("x_test.shape: ", self.x_test.shape)
         self.y_pred = regr.predict(self.x_test) #.reshape(-1)
+        #quit()
     
         # The coefficients
         #print('Coefficients: \n', regr.coef_)
@@ -245,6 +256,8 @@ class Trading:
         # The mean squared error
         # make sure that y_test and y_pred have same shape)
         # https://scikit-learn.org/stable/modules/feature_selection.html
+
+        self.r2_score = r2_score(self.y_test, self.y_pred)
 
         #self.printMetrics()
         u.plotPredVsRealPrice(self.sym, self.x_test[:,0], self.y_test, self.y_pred)
@@ -260,24 +273,25 @@ class Trading:
     
         all_days_total_real_profit = real_profit.sum()
         all_days_total_pred_profit = pred_profit.sum()
+
         print("***** count all the days ****")
         print("total_real_profit= ", all_days_total_real_profit)
         print("total_pred_profit= ", all_days_total_pred_profit)
     
         # Only count profit on days where the predicted profit is positive
-        print("profit_thresh= ", self.profit_thresh)
+
         profit = 100.*(pred_profit / self.x_test[:,0]) > profit_thresh
         real_profit = real_profit[profit]
         pred_profit = pred_profit[profit]
         profit_dates = self.test_dates[profit]
     
+        print("shapes: x_test, real_profit, prd_profit, profit_dates")
         print(self.x_test.shape, real_profit.shape, pred_profit.shape, profit_dates.shape)
-        print("test_dates: ", self.test_dates.values[0:10])
+        print("test_dates: ", self.test_dates[0:5])
         print("Profit_shape: ", profit.shape) # 1D array
-        print("real_profit= ", real_profit[0:10])
-        print("pred_profit= ", pred_profit[0:10])
-        print("profit dates= ", profit_dates[0:10])
-        #print("close[profit_dates]= ", x_test[profit,0])
+        print("real_profit= ", real_profit[0:5])
+        print("pred_profit= ", pred_profit[0:5])
+        print("profit dates= ", profit_dates[0:5])
     
         # Sum up the real profit over the test data
         total_real_profit = real_profit.sum()
@@ -286,7 +300,9 @@ class Trading:
         print("total_real_profit= ", total_real_profit)
         print("total_pred_profit= ", total_pred_profit)
 
-        self.total_profit = total_real_profit
+        self.total_real_profit = total_real_profit
+        self.total_pred_profit = total_pred_profit
+        #quit()
 
     #---------------------------
     def printMetrics(self):
@@ -297,14 +313,17 @@ class Trading:
         print("Features (put in Pandas df): ", self.feature_names)
         print('Mean squared error: %.2f' % mean_squared_error(self.y_test, self.y_pred))
         print("Explained variance score: ", explained_variance_score(self.y_test, self.y_pred))
+
         mi = mutual_info_regression(self.x_train, self.y_train)
         mi = mi / np.max(mi)
-        print("Mutual Information: ", mi)
+
         fr, pval = f_regression(self.x_train, self.y_train, center=True)  # center?
+
+        print("Mutual Information: ", mi)
         #fr = fr / np.max(fr)
         print("f_regression: " , fr)
         print("pval: " , pval)
-        print("R2 score: ", r2_score(self.y_test, self.y_pred))
+        print("R2 score: ", self.r2_score)
         print("=========== END METRICS =========================")
         print("=================================================")
         print()
@@ -314,13 +333,16 @@ class Trading:
 #----------------------------------------------------------------------
 stocks = ["AAPL", "ZEUS"]
 stocks = ["ZEUS"]
+stocks = ["ZBRA"]
+stocks = ["SENEB"]
 stocks = ["INDY"]
 stocks = ["AAPL"]
-stocks = ["ZBRA"]
 #stocks = ["AAPL", "ZEUS", "ZBRA", "INDY"]
 
 # Keep data up to 2018 for training
 break_date = 20150701
+break_date = 20160701
+break_date = 20170705
 folder = "symbols/"
 
 # nb_train_days is decreased by wait_days
@@ -330,26 +352,41 @@ nb_train_days = [200]
 
 # nb of days to use for the features
 # the same features are used for each day
-nb_feature_days = 5
+nb_feature_days_list = [1,5,10]
 profit_thresh   = 0  # percentage profit below which I do not enter the trade
-wait_days = 1 # how many days in the future to measure the profit 
+wait_days_list = [1,5,10,20,40] # how many days in the future to measure the profit 
+wait_days_list = [1,5] # how many days in the future to measure the profit 
 
 
-cols = ['sym', 'break_date', 'ndays', 'nb_feature_days', 'wait_days', 'profit_thresh', 'profit']
+cols = ['sym', 'break_date', 'ndays', 
+        'nb_feature_days', 'wait_days', 
+        'profit_thresh', 'pred_profit', 'real_profit', 
+        'nb_train_days', 'nb_test_days', 'r2_score']
 df = pd.DataFrame(columns=cols)
 records = []
 
 
 for sym in stocks:
-  for ndays in nb_train_days:
-    #for wait_days in [1, 5, 10, 20]:
-    for wait_days in [5]:
-        # profit_thresh is in dollars
-        trade = Trading(folder, break_date, ndays, nb_feature_days, wait_days, profit_thresh)
-        trade.getTrainTest(sym)
-        trade.processStock()
-        records.append([sym, break_date, ndays, nb_feature_days, wait_days, profit_thresh, trade.total_profit])
+    for ndays in nb_train_days:
+        #for wait_days in [1, 5, 10, 20]:
+        for wait_days in wait_days_list:
+            for nb_feature_days in nb_feature_days_list:
+                # profit_thresh is in dollars
+                print("---------------------------------------")
+                print("wait_days: ", wait_days)
+                print("nb_feature_days: ", nb_feature_days)
+                print("nb_train_days: ", nb_train_days)
+                trade = Trading(folder, break_date, ndays, nb_feature_days, wait_days, profit_thresh)
+                ret = trade.getTrainTest(sym)
+                if ret == False: 
+                    break
+                trade.processStock()
+                records.append([sym, break_date, ndays, 
+                                nb_feature_days, wait_days, 
+                                profit_thresh, trade.total_pred_profit, trade.total_real_profit, 
+                                trade.nb_train_days, trade.nb_test_days, trade.r2_score])
 
-dfnew = pd.DataFrame(records, columns=cols)
-df = pd.concat([df, dfnew])
-print(df)
+dfnew = pd.DataFrame(records, columns=cols) # 1st way
+print(dfnew)
+#df = pd.concat([df, dfnew])  # 2nd day, same result
+#print(df)
